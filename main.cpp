f9e72bb7224faed1e7d167bcec3102920964a2ad
Nikita Vetluzhskikh <trgrusted@gmail.com>
2024-12-25 22:47:32 +0100

    replace logic from main to new class

--- main.cpp
+++ main.cpp
@@ -1,35 +1,10 @@
 
-#include <iostream>
-#include <signal.h>
-#include <vector>
 
-#include "map.h"
-#include "robot.h"
+#include "SimulationController.h"
 
 int main(){
-  GeneticThings::Map *map = new GeneticThings::Map(8,50);
 
-  std::vector<GeneticThings::Robot*> robots;
+  auto sim = SimulationController();
 
-  for (int i = 0; i < 10; i++) {
-    auto test_robot = map->addRobot_at_random_place(i);
-    if (!test_robot)
-      break;
-    robots.push_back(test_robot);
-
-  }
-  map->printMap();
-
-  for (int i = 0; i < 10; i++) {
-    for (int j = 0; j < robots.size(); j++) {
-      robots[j]->execute_action();
-    }
-    sleep(1);
-    map->printMap();
-  }
-
-
-
-  delete map;
   return 0;
 }
